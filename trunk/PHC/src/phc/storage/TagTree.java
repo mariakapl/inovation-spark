b932ce521fa4e0f554f77004ca35a04c05e04ff1
maria.kaplinsky@gmail.com <maria.kaplinsky@gmail.com@f58b551f-dc3b-f19d-1595-715b439fbc06>
2014-07-26 17:14:26 +0000

    the tag tree

--- trunk/PHC/src/phc/storage/TagTree.java
+++ trunk/PHC/src/phc/storage/TagTree.java
@@ -1,5 +1,64 @@
 package phc.storage;
 
+import java.util.ArrayList;
+
 public class TagTree {
 
+	private Node root;
+	
+    public TagTree(String tag_name) {
+        root = new Node(tag_name);
+        root.children = new ArrayList<Node>();
+    }
+    
+    public Node findTag(String tag_name)
+    {
+    	if(root != null)
+    		return root.findTag(tag_name);
+    	return null;
+    }
+    
+	public static class Node {
+
+	    private String identifier;
+	    private ArrayList<Node> children;
+
+	    // Constructor
+	    public Node(String identifier) {
+	        this.identifier = identifier;
+	        children = new ArrayList<Node>();
+	    }
+
+	    public Node findTag(String tag_name) {
+			if(identifier == tag_name)
+			{
+				return this;
+			}
+			Node found = null; 
+			for(Node node : children)
+			{
+				if(found == null)
+					found = node.findTag(tag_name);
+			}
+			return found;
+		}
+
+		// Properties
+	    public String getIdentifier() {
+	        return identifier;
+	    }
+
+	    public ArrayList<Node> getChildren() {
+	        return children;
+	    }
+
+	    // Public interface
+	    public void addChild(String identifier) {
+	        children.add(new Node(identifier));
+	    }
+	}
+	
+	
+	
+	
 }
