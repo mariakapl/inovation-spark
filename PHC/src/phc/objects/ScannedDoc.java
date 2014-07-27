e1888f8cacd82a7460030ffcdbbdfa63efb016de
sreinst1 <sreinst1@gmail.com>
2014-07-27 10:33:27 +0000

    Add a name to each ScannedDoc

--- PHC/src/phc/objects/ScannedDoc.java
+++ PHC/src/phc/objects/ScannedDoc.java
@@ -7,15 +7,22 @@ import android.graphics.Bitmap;
 
 public class ScannedDoc {
 
+	private String _name;
 	private Bitmap _bitmap;
 	private String _ocr;
 	private Collection<String> _tags;
 	
-	public ScannedDoc(Bitmap bitmap, Collection<String> tags, String ocr)
+	public ScannedDoc(String name, Bitmap bitmap, Collection<String> tags, String ocr)
 	{
+		_name = name;
 		_bitmap = bitmap;
 		_tags = new HashSet<String>(tags);
 		_ocr = ocr;
+	}
+	
+	public String name()
+	{
+		return _name;
 	}
 	
 	public Bitmap bitmap()
