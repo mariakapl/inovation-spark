1d2651f71fd2a5f2eac0e5d98e6ca75683d1b224
sreinst1 <sreinst1@f58b551f-dc3b-f19d-1595-715b439fbc06>
2014-08-07 09:08:30 +0000

    Add more blood tests

--- PHC/src/phc/objects/BloodTest.java
+++ PHC/src/phc/objects/BloodTest.java
@@ -1,13 +1,15 @@
 package phc.objects;
 
 public class BloodTest {
-	public BloodTest(String name, String value, String units)
+	public BloodTest(String name, String value, String units, String date)
 	{
 		Name = name;
 		Value = value;
 		Units = units;
+		Date = date;
 	}
 	public String Name;
 	public String Value;
 	public String Units;
+	public String Date;
 }
--- PHC/src/phc/processing/BloodTestProcessor.java
+++ PHC/src/phc/processing/BloodTestProcessor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import phc.objects.BloodTest;
 import phc.objects.DocResult;
 import phc.utils.Utils;
 import android.content.Context;
@@ -15,14 +16,15 @@ public class BloodTestProcessor implements IDocProcessor {
 	private static final String TXT_EXTENSION = ".txt";
 
 	private static final String [] _tests = {
-		"Hemoglobin"
+		"Hemoglobin", "Glucose", "Platelets"
 	};
 	private HashMap<String, Pattern> _patterns;
 
 	public BloodTestProcessor() {
 		_patterns = new HashMap<String, Pattern>();
 		for (String test : _tests) {
-			Pattern p = Pattern.compile("\\b(" + test.toLowerCase() + ")\\s+((\\d|\\.)+)");
+			Pattern p = Pattern.compile(
+				"\\b(" + test.toLowerCase() + ")\\s+((\\d|\\.)+)\\s+(.*)");
 			_patterns.put(test, p);
 		}
 	}
@@ -51,31 +53,36 @@ public class BloodTestProcessor implements IDocProcessor {
 		return values;
 	}
 	
-	public void writeData(Context context, String test, String value, String date)
+	public void writeData(Context context, BloodTest test)
 	{
 		File dir = getDir(context);
-		File file = new File(dir, test + TXT_EXTENSION);
+		File file = new File(dir, test.Name + TXT_EXTENSION);
 		String s = "";
 		if (file.exists())
 			s = Utils.readTextFileAsString(file);
-		s = s + value + "\t" + date + "\n";
+		s = s + test.Value + "\t" + test.Units + "\t" + test.Date + "\n";
 		Utils.writeTextFile(file, s);
 	}
 	
 	@Override
 	public boolean process(Context context, DocResult doc) {
-		String ocr = doc.scannedDoc().ocr();
-		HashMap<String, String> matches = new HashMap<String, String>();
+		String ocr = doc.scannedDoc().ocr().toLowerCase();
+		HashMap<String, BloodTest> matches = new HashMap<String, BloodTest>();
 		for (String test : _patterns.keySet()) {
 			Pattern p = _patterns.get(test);
 			Matcher m = p.matcher(ocr);
-			if (m.find())
-				matches.put(test, m.group(2));
+			if (m.find()) {
+				BloodTest bt = new BloodTest(test, m.group(2), m.group(4), doc.date());
+				matches.put(test, bt);
+			}
 		}
 		if (matches.size() > 0)
 		{
-			for (String m : matches.keySet())
-				writeData(context, m, matches.get(m), doc.date());
+			for (String m : matches.keySet()) {
+				BloodTest test = matches.get(m);
+				writeData(context, test);
+				doc.addBloodTest(test);
+			}
 		}
 		return true;
 	}
